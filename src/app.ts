2278b979aa35ee58d213d261d0472d2f802851dd
ganeshmkharvi <102376190+ganeshmkharvi@users.noreply.github.com>
2022-04-23 17:02:43 +0300

    cors issue resolved
    cors issue resolved

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import * as dbConfig from './config/database';
 import express from "express";
 import {userRoute} from "./routes/user";
+import cors from "cors";
 
 dbConfig.connect();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 userRoute(app);
